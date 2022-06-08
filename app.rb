989292497b5340e54cc5c9568799bcd248a0f130
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-06-08 12:49:19 +0200

    First commit - Created folders structure to figurate MVC approach + reused own old Ruby OO code from on of the instance of the recurring Gossip Project + Added Gemfile + Added .gitignore

---
+++ app.rb
@@ -0,0 +1,12 @@
+# app.rb - Coded with love & sweat by Jean-Baptiste VIDAL for THP Developer curriculum (Winter 2022)
+# W19D3~5 - Final POO project
+
+# Useful includes & requires
+require 'bundler'
+Bundler.require
+require_relative './router/router.rb'
+
+# Launches the whole program logic by invoking the perform method on the newly created Router object
+Router.new.perform
+
+# app.rb - Coded with love & sweat by Jean-Baptiste VIDAL for THP Developer curriculum (Winter 2022)
---
+++ router/router.rb
@@ -0,0 +1,63 @@
+# router.rb - Coded with love & sweat by Jean-Baptiste VIDAL for THP Developer curriculum (Winter 2022)
+# W19D3~5 - Final POO project
+
+# Useful includes
+require_relative '../controller/controller.rb'
+require_relative '../view/show.rb'
+
+# Router - Class branching the user to the relevant Controllers and/or Views to enable interacting wit the Model #mvcpowaa
+class Router
+
+  def initialize
+    @controller =  Controller.new("data/gossip.csv")
+  end 
+
+  # This is where all the routing magic happens... Sorry for using a "case...when", I know how much sore this is :-)
+  def perform 
+    tmp_prompt = []
+    tmp_gossip_id = 0
+    tmp_route = false
+    exit = false
+    Show.clrscr
+    Show.disp("WELCOME TO THE GOSSIP PROJECT")
+    while !exit
+      choice = Show.display_menu
+      case choice
+      when 1 # CREATE a new gossip
+        Show.disp("You chose to store a new gossip. Cool!")
+        tmp_prompt = Show.input_gossip
+        @controller.create_gossip(tmp_prompt[0], tmp_prompt[1])
+      when 2 # READ all stored gossips
+        Show.disp("You decided to have a look at our gossip warehouse")
+        tmp_route = @controller.index_gossips
+      when 3 # UPDATE an existing gossip [Work in progress]
+        Show.disp("Hey, you! The UPDATE feature for our gossip list is still WIP. Keep calm, lay back and eat a pizza instead.")
+      when 4 # DELETE a given gossip
+        Show.disp("Yo, Stranger! This feature is not for lamerz... Be careful with what you do to my gossip list, OK?")
+        Show.disp("")
+        tmp_route = @controller.index_gossips
+        if tmp_route
+          tmp_gossip_id = Show.prompt_for_deletion
+          if @controller.delete_gossip(tmp_gossip_id)
+            Show.disp("Nicely done, Stranger! Looks like gossip ##{tmp_gossip_id} has been deleted. Surely telling unmentionable things about you ;-)")
+            Show.pause
+          else
+            Show.disp("Oooops! Looks like something went wrong... Next time, ask your 3-year-old brother or sister for help, Stranger.")
+            Show.pause
+          end
+        else
+          Show.disp("Looks like the Gossip file is still empty: no mean to delete from it, then... You fool :-D")
+          Show.pause
+        end
+      when 5
+        Show.disp("")
+        Show.disp("Hope to see you (not too) soon, Stranger!")
+        exit = true
+      else
+        Show.disp("You apparently encountered difficulties typing an adequate choice... Ask your dog or cat for help ;-)")
+      end
+    end
+  end
+end
+
+# router.rb - Coded with love & sweat by Jean-Baptiste VIDAL for THP Developer curriculum (Winter 2022)
